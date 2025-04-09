a73a3862e3b6aab0a4be70fbfa262089215e87a2
Jitin Gupta <136317512+jitingupta5072008@users.noreply.github.com>
2025-04-09 10:49:57 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,2 +1,2 @@
-export const USER_API_END_POINT = "https://fastro-in-backend.onrender.com"
-export const SELLER_API_END_POINT = "https://fastro-in-backend.onrender.com"
+export const USER_API_END_POINT = "https://fastro-in-backend.onrender.com/api/v1/user"
+export const SELLER_API_END_POINT = "https://fastro-in-backend.onrender.com/api/v1/user"
