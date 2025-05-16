b18b11bf68d9a54eeb9d3e9791c3359b9863d6c8
jitingupta5072008 <jitingupta5072008@gmail.com>
2025-05-16 15:19:55 +0530

    add order-success page

--- Frontend/src/Pages/Order/OrderSuccess.jsx
+++ Frontend/src/Pages/Order/OrderSuccess.jsx
@@ -1,12 +1,30 @@
 import { CircleCheck } from 'lucide-react'
-import React from 'react'
-import { useLocation, useNavigate } from 'react-router-dom';
-
+import React, { useEffect, useState } from 'react'
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import axios from 'axios'
+import { USER_API_END_POINT } from '../../utils/api';
+import toast from 'react-hot-toast';
 const OrderSuccess = () => {
-  const location = useLocation();
-  const order = location.state?.order[0];
-  const navigate = useNavigate()
-  console.log(order);
+  const {orderId} = useParams()
+  const [order,setOrder] = useState(null)
+  const [loading,setLoading] = useState(true)
+
+
+  useEffect(()=>{
+    const fetchOrder = async ()=>{
+      try {
+        const res = await axios.get(`${USER_API_END_POINT}/orders_detail/${orderId}`);
+        setOrder(res.data);
+        setLoading(false)
+      } catch (error) {
+        setLoading(false)
+        toast.error(error || 'failed to fetch order')
+      }
+    }
+    fetchOrder()
+  },[orderId])
+
+  if(loading) return <p>Loading...</p>
 
   return (
     <div className="min-h-screen flex items-center justify-center" style={{ background: 'linear-gradient(to right, #ffe4f1, #ffffff)'}}>
@@ -46,14 +64,14 @@ const OrderSuccess = () => {
         <div className="bg-pink-50 rounded-xl p-4 text-left mb-6 border border-pink-100">
           <h2 className="text-2xl font-semibold text-gray-800 mb-4">Your Products</h2>
           <div className="space-y-4">
-            {order.products && order.products.map((product, index) => (
+            {order.items && order.items.map((product, index) => (
               <div key={index} className="flex justify-between border-b pb-2">
                 <div>
                   <p className="font-medium text-gray-700">{product.name}</p>
                   <p className="text-gray-500">Quantity: {product.quantity}</p>
                 </div>
                 <div className="text-right">
-                  <p className="text-gray-700 font-semibold">₹{product.price * product.quantity}</p>
+                  <p className="text-gray-700 font-semibold">₹{order.totalAmount}</p>
                 </div>
               </div>
             ))}
